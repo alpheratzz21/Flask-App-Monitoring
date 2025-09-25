fe5aa118ec12e7162b5fa769a49704e3bb6334e3
alpheratz <rifqi@Rifqi.localdomain>
2025-09-25 22:21:26 +0700

    update app.py add metrics counter

--- app/app.py
+++ app/app.py
@@ -1,20 +1,30 @@
-from flask import Flask
-from prometheus_client import Counter, generate_latest
+from flask import Flask, request
+from prometheus_client import Counter, Histogram, generate_latest
+import time
 
 app = Flask(__name__)
 
 # counter metric for counting request
 REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests", ["endpoint"])
 
+# histogram metric for latency
+REQUEST_LATENCY = Histogram("http_request_latency_seconds", "Request latency", ["endpoint"])
+
 @app.route("/")
 def hello_world():
-    REQUEST_COUNT.labels(endpoint="/").inc()  # every request increase counter
-    return {"message": "Hello, World from Flask!"}
+    start = time.time()
+    REQUEST_COUNT.labels(endpoint="/").inc()
+    response = {"message": "Hello, World from Flask!"}
+    REQUEST_LATENCY.labels(endpoint="/").observe(time.time() - start)
+    return response
 
 @app.route("/health")
 def health():
+    start = time.time()
     REQUEST_COUNT.labels(endpoint="/health").inc()
-    return {"status": "ok"}
+    response = {"status": "ok"}
+    REQUEST_LATENCY.labels(endpoint="/health").observe(time.time() - start)
+    return response
 
 # special endpoint to expose metrics
 @app.route("/metrics")
