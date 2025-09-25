3f14d78e7adcfa257644bc364c90d8883f63c69a
alpheratz <rifqi@Rifqi.localdomain>
2025-09-25 23:57:37 +0700

    update counter/endpoint and req latency

--- app/app.py
+++ app/app.py
@@ -4,29 +4,29 @@ import time
 
 app = Flask(__name__)
 
-# counter metric for counting request
-REQUEST_COUNT = Counter("http_requests_total", "Total HTTP Requests", ["endpoint"])
+# counter metric using label endpoint
+REQUEST_COUNT = Counter(
+    "http_requests_total", "Total HTTP Requests", ["endpoint"]
+)
 
-# histogram metric for latency
-REQUEST_LATENCY = Histogram("http_request_latency_seconds", "Request latency", ["endpoint"])
+# histogram latency
+REQUEST_LATENCY = Histogram(
+    "http_request_latency_seconds", "Request latency", ["endpoint"]
+
+)
 
 @app.route("/")
 def hello_world():
-    start = time.time()
-    REQUEST_COUNT.labels(endpoint="/").inc()
-    response = {"message": "Hello, World from Flask!"}
-    REQUEST_LATENCY.labels(endpoint="/").observe(time.time() - start)
-    return response
+    with REQUEST_LATENCY.labels(endpoint="/").time():
+        REQUEST_COUNT.labels(endpoint="/").inc()
+        return {"message": "Hello, World from Flask!"}
 
 @app.route("/health")
 def health():
-    start = time.time()
-    REQUEST_COUNT.labels(endpoint="/health").inc()
-    response = {"status": "ok"}
-    REQUEST_LATENCY.labels(endpoint="/health").observe(time.time() - start)
-    return response
+    with REQUEST_LATENCY.labels(endpoint="/health").time():
+        REQUEST_COUNT.labels(endpoint="/health").inc()
+        return {"status": "ok"}
 
-# special endpoint to expose metrics
 @app.route("/metrics")
 def metrics():
     return generate_latest(), 200, {"Content-Type": "text/plain; charset=utf-8"}
